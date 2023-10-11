3b45ae84eb3d3393f8a7dfc10492c5fc338268f3
carlosmilenaquesada_portatil <carlosmilenaquesada@gmail.com>
2023-10-11 19:33:18 +0200

    ultimo

--- app/src/main/java/com/example/promul_chat_con_dos_activities_activity_result/SecondActivity.java
+++ app/src/main/java/com/example/promul_chat_con_dos_activities_activity_result/SecondActivity.java
@@ -2,13 +2,33 @@ package com.example.promul_chat_con_dos_activities_activity_result;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    TextView textoChatsecond;
+    EditText mensajeAEnviarPorSecond;
+    Button enviarSecond;
+    public static String ENVIADO_POR_SECOND = "promul_chat_con_dos_activities_activity_result.SecondActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        textoChatsecond = (TextView)  findViewById(R.id.textoChatSecond);
+        mensajeAEnviarPorSecond = (EditText) findViewById(R.id.mensajeAEnviarPorSecond);
+        enviarSecond = (Button) findViewById(R.id.enviarSecond);
+    }
+
+    public void enviarMensaje(View view){
+        String mensaje = String.valueOf(mensajeAEnviarPorSecond.getText());
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(ENVIADO_POR_SECOND, mensaje);
+        startActivity(intent);
     }
 }
