6d36125a4880436ba27f037997aabddd5ca6173f
carlosmilenaquesada <135043776+carlosmilenaquesada@users.noreply.github.com>
2023-10-11 13:04:02 +0200

    primero

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.promul_chat_con_dos_activities_activity_result"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.promul_chat_con_dos_activities_activity_result"
